f31181bcb070b81f294d45c6bac9fa541af6a8fa
razamob <razamob@sheridancollege.ca>
2020-07-22 23:44:01 -0400

    Final fix host

--- trv/settings.py
+++ trv/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'wn^s8d-of-*9@w@&2nt8-1kf$*p2$+fc8ihb8oct3is8^50-dp'
 DEBUG = True
 
 ALLOWED_HOSTS = ['web-app-dev.us-east-2.elasticbeanstalk.com',
-                 'localhost', '172.31.5.94']
+                 'localhost', '172.31.30.196']
 
 
 # Application definition
