0a4c5d2a38c3d9ba8d0018e42096b3dd67afe1b5
igbinosa idahosa <iidahosa@bell.net>
2020-10-14 14:04:32 -0400

    Working on filter data usecase

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,14 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('logout', views.logout, name="logout"),
-    path('dashboard', views.dashboard, name="dashboard")
+    path('dashboard', views.dashboard, name="dashboard"),
+
+
+    path('manageData', views.manage_data, name="manageData"),
+    path('filterData', views.filter_data, name="filterData"),
+    path('editmeetingcomments', views.edit_meeting_comments, name="editmeetingcomments"),
+    # is it cus they had thesame name?
+    path('filterDatachange', views.check_frequency, name="check-frequency")
+
+    #path('filterdatasearch', views.filter_data_search, name="filterdatasearch")
 ]
