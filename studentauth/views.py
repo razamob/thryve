8a193f4d8ff1e9a561981b8901a3235e58437327
Thea Celestino <therealme.theac@gmail.com>
2020-10-16 01:49:38 -0400

    studentauth testing

--- studentauth/views.py
+++ studentauth/views.py
@@ -32,7 +32,7 @@ class StudentAuthView(viewsets.ModelViewSet):
         serializer = StudentAuthSerializer(studentauth)
         studentauths = StudentAuth.objects.all()
         #return render(request, 'studentauths/studentauths.html', {'studentauths': studentauths})
-        return JsonResponse({"studentauths": list(queryset)})
+        return JsonResponse({'studentauths': studentauths})
 
     # def destroy(self, request, pk):
     #     studentauth = StudentAuth.objects.get(id=pk)
