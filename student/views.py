022ac8bfdd57a123cbb00e962cce4dfdf9f17967
Thea Celestino <therealme.theac@gmail.com>
2020-10-22 12:03:07 -0400

    student account test

--- student/views.py
+++ student/views.py
@@ -43,7 +43,7 @@ class StudentAccountView(viewsets.ModelViewSet):
         )
         serializer = StudentAccountSerializer(studentaccount)
         studentaccounts = StudentAccount.objects.last().values()
-        return JsonResponse({"studentaccounts": list(studentaccount)})
+        return JsonResponse({"studentaccounts": list(studentaccounts)})
 
 
     # def destroy(self, request, pk):
